a5e664df6539e76618f442c632c827961750baa0
Monika-koduje <69014445+Monika-koduje@users.noreply.github.com>
2021-02-01 00:27:47 +0100

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,81 @@
+// click - background button
+$(".background_1").click(function () {
+  $("img.bg_1").toggleClass("img_click_bg");
+  $("button.background_1").toggleClass("bg_click");
+});
+
+$(".background_2").click(function () {
+  $("img.bg_2").toggleClass("img_click_bg");
+  $("button.background_2").toggleClass("bg_click");
+});
+
+$(".background_3").click(function () {
+  $("img.bg_3").toggleClass("img_click_bg");
+  $("button.background_3").toggleClass("bg_click");
+});
+
+$(".background_4").click(function () {
+  $("img.bg_4").toggleClass("img_click_bg");
+  $("button.background_4").toggleClass("bg_click");
+});
+
+// click - snowflakes button
+$(".sn_1").click(function () {
+  $("img.snow_1").toggleClass("img_click_sn");
+  $("button.sn_1").toggleClass("sn_click");
+});
+
+$(".sn_2").click(function () {
+  $("img.snow_2").toggleClass("img_click_sn");
+  $("button.sn_2").toggleClass("sn_click");
+});
+
+$(".sn_3").click(function () {
+  $("img.snow_3").toggleClass("img_click_sn");
+  $("button.sn_3").toggleClass("sn_click");
+});
+
+$(".sn_4").click(function () {
+  $("img.snow_4").toggleClass("img_click_sn");
+  $("button.sn_4").toggleClass("sn_click");
+});
+
+// click - element button
+$(".el_1").click(function () {
+  $("img.element_1").toggleClass("img_click_el");
+  $("button.el_1").toggleClass("el_click");
+});
+
+$(".el_2").click(function () {
+  $("img.element_2").toggleClass("img_click_el");
+  $("button.el_2").toggleClass("el_click");
+});
+
+$(".el_3").click(function () {
+  $("img.element_3").toggleClass("img_click_el");
+  $("button.el_3").toggleClass("el_click");
+});
+
+$(".el_4").click(function () {
+  $("img.element_4").toggleClass("img_click_el");
+  $("button.el_4").toggleClass("el_click");
+});
+$(".el_5").click(function () {
+  $("img.element_5").toggleClass("img_click_el");
+  $("button.el_5").toggleClass("el_click");
+});
+
+$(".el_6").click(function () {
+  $("img.element_6").toggleClass("img_click_el");
+  $("button.el_6").toggleClass("el_click");
+});
+
+$(".el_7").click(function () {
+  $("img.element_7").toggleClass("img_click_el");
+  $("button.el_7").toggleClass("el_click");
+});
+
+$(".el_8").click(function () {
+  $("img.element_8").toggleClass("img_click_el");
+  $("button.el_8").toggleClass("el_click");
+});
